4ea6af0275d6e46eb530a9b940decfb7c72f05c6
Felipe Beltran <Anfebelo9712@gmail.com>
2025-03-01 10:24:21 -0500

    V0.4

--- program.js
+++ program.js
@@ -14,8 +14,6 @@ async function loadPolygon (){
         {
             style:{
                 color:'red'
-
-
             }
         }
     ).addTo(map);
@@ -23,27 +21,85 @@ async function loadPolygon (){
 
 loadPolygon();
 
-async function loadPoints (){
-    
-    let myData2 = await fetch('arboles_pardorubio.geojson');
-    let myPoint = await myData2.json();
-    
-    L.geoJSON(myPoint,
-        {
-            style:{
-                color:'red'
-
-
-            }
-        }
-    ).addTo(map);
-}
-
-loadPoints();
-
-
 
 
 let btnTrees = document.getElementById("btnTrees");
 
-btnTrees.addEventListener('click', ()=>alert("Hola"));
+btnTrees.addEventListener('click', 
+    async function(){
+        let response = await fetch("arboles_pardorubio.geojson");
+        let datos = (await response.json());
+        //Agregar la capa al mapa
+        L.geoJSON(
+            datos,
+            {
+                pointToLayer: (feature, latlong)=>{
+
+                    return L.circleMarker(latlong, {
+                        radius:3,
+                        fillColor:'green',
+                        weight:1,
+                        opacity:1,
+                        fillOpacity: 0.5,
+                    })
+
+                }
+            }
+        ).addTo(map);
+
+    }
+    
+)
+
+let btnDistance = document.getElementById("btnDistance");
+
+btnDistance.addEventListener('click', 
+    async function(){
+        let response = await fetch("arboles_pardorubio.geojson");
+        let datos = await response.json();
+        let trees = datos.features.map((myElement, index)=>({
+            id: index+1,
+            coordinates: myElement.geometry.coordinates
+        }));
+
+        
+        let distances=[];
+        trees.forEach( (treeA)=>{trees.forEach(
+
+            
+                (treeB)=>{
+                    if(treeA.id != treeB.id){
+                        let distance = turf.distance( 
+                            turf.point(treeA.coordinates),
+                            turf.point(treeB.coordinates),
+                        );
+                        distances.push(
+                            [
+                                `Árbol ${treeA.id}`,
+                                `Árbol ${treeB.id}`,
+                                distance.toFixed(3)                            
+                            ]
+                        )
+                }
+            }
+            )
+        }
+        )
+        generatePDF(distances, trees.lenght);
+    }
+)
+
+function  generatePDF(distances, totalTrees){
+    let { jsPDF } = window.jspdf;
+    let documentPDF = new jsPDF();
+    documentPDF.text("Reporte de arboles barrio pardo rubio",10,10);    
+    
+    documentPDF.autoTable(
+        {
+        head: [['Arbol 1', 'Arbol 2', 'Distance']],
+        body: distances
+        }
+    );
+    documentPDF.save("PardoRubio.pdf")
+
+}
